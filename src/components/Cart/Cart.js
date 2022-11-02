f2bdccce91943b6dee7ab5fdf6814235a46addf0
Naman Jain <namanjain15032001@gmail.com>
2022-11-02 19:38:56 +0530

    format

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,7 @@ import CartItem from './CartItem/CartItem';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { coupons } from '../../constants/coupons';
-import speaker from '../Products/speaker.jpg'
+import speaker from '../Products/speaker.jpg';
 
 function Cart1({ cart = [] }) {
 
